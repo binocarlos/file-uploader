46628ef3f9c6d199400a685caeb08902c841189d
Kai Davenport <kaiyadavenport@gmail.com>
2014-01-26 12:19:14 +0000

    non-digger

--- index.js
+++ index.js
@@ -1,62 +1,73 @@
-var template = require('./template');
 var Dropzone = require('dropzone');
-
-$digger.directive('fileUploader', function($http, $safeApply){
-	return {
-    restrict:'EA',
-    template:template,
-    replace:true,
-    controller:function($scope){
-
-			
-    },
-
-    link:function($scope, elem, $attrs){
-
-      $scope.uploadid = $digger.littleid();
-      $scope.uploadurl = $digger.config.diggerurl + '/reception/files/upload'
-
-      if($scope.readonly){
-        
-      }
-      else{
-
-        var myDropzone = new Dropzone(elem.get(0), {
-          url: $digger.config.diggerurl + '/reception/files/upload',
-          maxFiles:1,
-          uploadMultiple:false,
-          addRemoveLinks:true,
-          clickable:true,
-          init: function() {
-            this.on("sending", function(file, xhr, formdata){
-              formdata.append('containerid', $scope.container.diggerid());
-              formdata.append('warehouse', $scope.container.diggerwarehouse());
-            })
-
-            this.on("success", function(file, responseText) {
-              $safeApply($scope, function(){
-                $scope.model[$scope.fieldname] = $scope.fileurl = $digger.config.diggerurl + '/reception/files' + responseText;
-                $scope.processfieldname();
-              })
-            });
-          }
-        });
-      }
-
-
-      $scope.processfieldname = function(){
-        var url = $scope.model[$scope.fieldname];
-        if(url){
-          var parts = url.split('/');
-          $scope.filename = parts.pop();
-        }
-      }
-
-      $scope.processfieldname();
-    	
-    }
-  }
-})
-
 Dropzone.autoDiscover = false;
-module.exports = '<file-uploader />';
+
+module.exports = 'file.uploader';
+
+angular
+  .module('file.uploader', [
+    require('digger-utils-for-angular')
+  ])
+
+  .directive('fileUploader', function($http, $safeApply){
+  	return {
+      restrict:'EA',
+      template:require('./template'),
+      replace:true,
+      link:function($scope, elem, $attrs){
+
+        $scope.uploadid = $digger.littleid();
+
+        var container_url = $scope.container.diggerwarehouse() + '/' + $scope.container.diggerid() + '/' + $scope.fieldname + '/' + $scope.uploadid;
+        var base_url = $attrs.url || $digger.config.diggerurl + '/reception/files';
+
+        $scope.file_url = base_url + container_url;
+
+        if($scope.readonly){
+          
+        }
+        else{
+
+          var myDropzone = new Dropzone(elem.get(0), {
+            url: $scope.file_url,
+            maxFiles:1,
+            uploadMultiple:false,
+            addRemoveLinks:true,
+            clickable:true,
+            init: function() {
+              this.on("sending", function(file, xhr, formdata){
+                //formdata.append('containerid', $scope.container.diggerid());
+                //formdata.append('warehouse', $scope.container.diggerwarehouse());
+              })
+
+              this.on("success", function(file, responseText) {
+                $safeApply($scope, function(){
+                  console.log('-------------------------------------------');
+                  console.log('-------------------------------------------');
+                  console.dir(responseText);
+                  $scope.model[$scope.fieldname] = $scope.file_url;
+                  $scope.processfieldname();
+                  if($attrs.autoname){
+                    $scope.model.name = $scope.filename;
+                  }
+                })
+              });
+            }
+          });
+        }
+
+
+        $scope.processfieldname = function(){
+          var url = $scope.model[$scope.fieldname];
+          if(url){
+            var parts = url.split('/');
+            $scope.filename = parts.pop();
+          }
+        }
+
+        $scope.processfieldname();
+      	
+      }
+    }
+  })
+
+
